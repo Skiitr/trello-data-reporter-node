5c4f366ac56e5d1da50f7302cc477287e308ca36
Dan Gallagher <DGallagher@Avidyne.com>
2016-02-16 08:48:24 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+##Trello Data Reporter##
+###by Dan Gallagher###
+
+This application was designed to extract data from an exported Trello .json file. Currently this list of features it displays are as follows:
+
+-Shows the total number of cards
+-Shows the number of closed cards
+-Shows the number of open cards
+
+To use this application you must first have an exported Trello board in a JSON format and have Node.js installed. For instructions on either of these two things see Trello and/or Node.js documentation.
+
+Once you have that you can run the exported trello board with the app.js file by having both files in the same directory, navagating to that directory and running:
+
+'''
+node app.js filename.json
+'''
---
+++ app.js
@@ -0,0 +1,22 @@
+//Trello Data Reporter for an exported board in JSON using Node.js
+//by Dan Gallagher
+
+//Require file system in node, read in the input file, and parse the JSON
+var fs = require('fs'),
+	content = fs.readFileSync(process.argv[2]),
+	jsonData = JSON.parse(content)
+
+//Count the cards/issues that are closed
+var closedCards = 0
+for (var i = 0; i < jsonData.cards.length; i++) {
+	if (jsonData.cards[i].closed === true) { closedCards++ }
+}
+
+//Print the total number of cards/issues
+console.log('This board contains ' + jsonData.cards.length + ' cards:')
+
+//Print the number of closed cards/issues
+console.log(closedCards + ' are closed,')
+
+//Print the number of closed cards/issues
+console.log(jsonData.cards.length - closedCards + ' are open.')
